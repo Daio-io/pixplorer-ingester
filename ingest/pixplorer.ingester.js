c9a5aa53b0fabe66aa33fbb8ae98e2a04d1bc000
Daveloper87 <dai.williams@bbc.co.uk>
2015-05-07 23:11:11 +0100

    added word ingest script and word lis

---
+++ ingest/pixplorer.ingester.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var WordModel = require('./words.model');
+var fs = require('fs');
+
+exports.startIngest = function() {
+
+    fs.readFile('./ingest/words.json', function (err, data) {
+
+        var wordsList = JSON.parse(data);
+
+        wordsList.map(function (item) {
+
+            new WordModel({word: item.word}).save(function (err) {
+
+                console.log('Error adding word: ', item.word, err);
+
+            });
+
+        });
+
+    });
+
+};
